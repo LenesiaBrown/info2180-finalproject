e49018bed89ec1ce55c56bae3c58ca0b256b22a9
Kayla Swift <kayla.swift1302@gmail.com>
2022-12-04 20:55:27 -0500

    Added Contacts

---
+++ createcontact.php
@@ -0,0 +1,31 @@
+<?php
+$host = 'localhost';
+$username = 'project2_user';
+$password = 'password123';
+$dbname = 'dolphin_crm';
+
+$title =filter_var($_POST['title'],FILTER_SANITIZE_STRING);
+$Fname =filter_var($_POST['Fname'],FILTER_SANITIZE_STRING);
+$Lname =filter_var($_POST['Lname'],FILTER_SANITIZE_STRING);
+$email =filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+$telephone =filter_var($_POST['telephone'],FILTER_SANITIZE_STRING);
+$company =filter_var($_POST['company'],FILTER_SANITIZE_STRING);
+$type =filter_var($_POST['type'],FILTER_SANITIZE_STRING);
+$assignedto =filter_var($_POST['assign'],FILTER_SANITIZE_STRING);
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM contacts WHERE email='$email'");
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if(count($results)==0){
+    session_start();
+    $created_by= $_SESSION['id'];
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->beginTransaction();
+    $conn->exec("INSERT INTO contacts(title, firstname, lastname, email, telephone, company, type, assignedto, created_by, created_at) VALUES ('$title', '$Fname', '$Lname', '$email', '$telephone', '$company', '$type', '$assignedto', '$created_by', Current_Timestamp)");
+    $conn->commit();
+
+    echo 'Saved';
+}
+
---
+++ newcontact.php
@@ -0,0 +1,108 @@
+<?php session_start();
+     if (isset ($_SESSION['id'])){
+?>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>New Contact</title>
+    <link rel ="stylesheet" href="">
+    <script src=""></script>
+    <link rel ="stylesheet" href="">
+    <script src="createcontact.js"></script>
+    </head>
+    <body>
+        <?php include 'index.php';?>
+        <div class = "container">
+            <div class ="screen">
+                <div class ="buttons">
+                    <div><a href="dashboard.php"><i class ="home" aria-hidden="true"></i>Home</a></div>
+                    <div><a href="createcontact.php"><i class ="add-new-contact" aria-hidden="true"></i>New Contact</a></div>
+                    <div><a href="newUser.php"><i class ="users" aria-hidden="true"></i>Users</a></div>
+                    <hr>
+                    <div><a href=""><i class ="home" aria-hidden="true"></i>Logout</a></div>
+                </div>
+            </div>
+            <div class= "background">
+                <div class= "records">
+                    <h1>New Contact</h1>
+                    <div class="record2">
+                        <form method="post" id="form" action="">
+                            <div class="table">
+                                <div class="cell">
+                                    <label for="title">Title</label><br>
+                                    <select id="title" name="title">
+                                        <option value="Mr."></option>
+                                        <option value="Mrs."></option>
+                                        <option value="Ms."></option>
+                                        <option value="Dr."></option>
+                                        <option value="Prof."></option>
+                                    </select>
+                                </div>
+                                <div class="cel1"> 
+                                </div>
+                                    <div class="table">
+                                         <div class="cell"> 
+                                             <label for= "Fname">First Name</label>
+                                             <input type="text" name="Fname" id="Fname" required/>
+                                        </div>
+                                    </div>
+                                         <div class="cell"> 
+                                             <label for= "Lname">Last Name</label>
+                                             <input type="text" name="Lname" id="Lname" required/>
+                                        </div>
+                                    </div>
+                                    <div class="table">
+                                         <div class="cell"> 
+                                             <label for= "email">Email</label>
+                                             <input type="email" name="email" id="email" required/>
+                                        </div>
+                                    </div>
+                                         <div class="cell"> 
+                                             <label>Telephone number</label>
+                                             <input type="text" name="telephone" id="telephone" pattern ="[0-9]{3}-[0-9]{3}-[0-9]{4}" title="Telephone number must be in the format: XXX-XXX-XXXX"required/>
+                                        </div>
+                                    </div>
+                                    <div class="table">
+                                         <div class="cell"> 
+                                             <label for= "company">Company</label>
+                                             <input type="text" name="company" id="company" required/>
+                                        </div>
+                                    </div>
+                                    <div class="cell">
+                                        <label for="type">Type</label><br>
+                                            <select id="type" name="type">
+                                                <option value="SALES LEAD">Sales Lead</option>
+                                                <option value="SUPPORT">Support</option>
+                                            </select>    
+                                            </div>
+                                    </div>
+                                    <div class="table">
+                                         <div class="cell"> 
+                                             <label for= "assignedto">Assign To</label><br>
+                                             <select id="type" name="type">
+                                                <option value="1">Michael Scott</option>
+                                                <option value="2">Dwight Shrute</option>
+                                                <option value="3">Pam Beesley</option>
+                                                <option value="4">Angela Martin</option>
+                                                <option value="5">Kelly Kapoor</option>
+                                                <option value="6">Jim Halpert</option>
+                                            </select>
+                                        </div><br>
+                                    <div class ="cell"></div>
+                                    </div><br>
+                                    <div class="save-button">
+                                        <button id="save">Save</button>
+                                    </div>
+                                 </form>
+                            </div>
+                            </div>
+                            </div>
+                            </div
+                            <?php include 'note.php';?>
+    </body>
+</html>
+<?php }
+ else{
+     header('location:dashboardFilter.php');    
+ }?>
